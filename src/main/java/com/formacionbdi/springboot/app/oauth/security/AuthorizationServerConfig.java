f2564bee753d3373ea66870d78cb8324a3c42016
ReBedc <rebekadc@gmail.com>
2021-05-23 13:22:31 +0100

    servicio oauth spring security

---
+++ src/main/java/com/formacionbdi/springboot/app/oauth/security/AuthorizationServerConfig.java
@@ -0,0 +1,70 @@
+package com.formacionbdi.springboot.app.oauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder; 
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()")
+		.checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("frontendapp")
+		.secret(passwordEncoder.encode("12345"))
+		.scopes("read","write") // la app puede leer, modificar, borrar datos
+		.authorizedGrantTypes("password", "refresh_token") // cuando los usuarios existen en el backend: xa autenticar requiere user + pass
+		.accessTokenValiditySeconds(3600) // tiempo de validez del token
+		.refreshTokenValiditySeconds(3600);
+		/*.and()  // para varios clientes
+		.withClient("androidapp")
+		.secret(passwordEncoder.encode("12345"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password", "refresh_token") 
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600)*/
+		// Tipo de autenticación via página de login
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenCoverter = new JwtAccessTokenConverter();
+		tokenCoverter.setSigningKey("algun_codigo_secreto_aeiou");
+		return tokenCoverter;
+	}
+	
+	
+}

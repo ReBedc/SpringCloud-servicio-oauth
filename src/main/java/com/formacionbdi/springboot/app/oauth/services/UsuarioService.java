2f21e315130f1be09f68551f92db1f51ad10a9b7
ReBedc <rebekadc@gmail.com>
2021-05-30 13:28:43 +0100

    Manejo de errores capturar FeignException

--- src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 
+import feign.FeignException;
+
 @Service
 public class UsuarioService implements IUsuarioService, UserDetailsService {
 	
@@ -27,23 +29,28 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = client.findByUsername(username);
 		
-		if (usuario == null) {
+		try {
+					
+			Usuario usuario = client.findByUsername(username);
+			
+			List<GrantedAuthority> authorities = usuario.getRoles()
+					.stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority()))
+					.collect(Collectors.toList());
+			
+			log.info("Usuario autenticado: " + username);
+			
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(),
+					true, true, true, authorities);
+		
+		} catch (FeignException e) {
 			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
 			throw new UsernameNotFoundException("Error en el login, no existe el usuario '" 
 				+ username + "' en el sistema");
 		}
-		List<GrantedAuthority> authorities = usuario.getRoles()
-				.stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority()))
-				.collect(Collectors.toList());
 		
-		log.info("Usuario autenticado: " + username);
-		
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(),
-				true, true, true, authorities);
 	}
 
 	@Override
